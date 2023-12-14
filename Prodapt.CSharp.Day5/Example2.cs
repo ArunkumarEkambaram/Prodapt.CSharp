a2241446c70fd2be196bac360e31c105c6dfa829
ArunKumar <arunkumar.e@outlook.com>
2023-12-14 12:59:46 +0530

    Static variable example

--- Prodapt.CSharp.Day5/Example2.cs
+++ Prodapt.CSharp.Day5/Example2.cs
@@ -1,14 +1,41 @@
-﻿using System;
-using System.Linq;
-
-namespace Prodapt.CSharp.Day5
+﻿namespace Prodapt.CSharp.Day5
 {
     internal class Example2
     {
+        //Instance Varaible or Non-Static Variable
+        int num1 = 10;
+
+        //Static variable
+       public static int num2 = 100;
+
+
         static void Main(string[] args)
         {
-            string a = "Hello";
-            Console.WriteLine(a.ToLower().Contains("he"));
+            Example2 obj1 = new Example2();
+            Console.WriteLine(obj1.num1);
+            obj1.num1 = 100;
+            Console.WriteLine(obj1.num1);
+            Console.WriteLine();
+            Example2 obj2 = new Example2();
+            Console.WriteLine(obj2.num1);
+            obj2.num1 = 5000;
+            Console.WriteLine(obj2.num1);
+            Console.WriteLine("Static Example");
+            Console.WriteLine($"Static Value :{Example2.num2}");
+            Example2.num2 = 2222;
+            Console.WriteLine($"Static Value :{Example2.num2}");
+            Console.WriteLine() ;
+            ExecuteClass obj = new ExecuteClass();
+            obj.Manipulate();
+        }
+    }
+
+    public class ExecuteClass
+    {
+        public void Manipulate()
+        {
+            Example2.num2 = 100000;
+            Console.WriteLine($"Static Value in Another Class :{Example2.num2}");
         }
     }
 }
---
+++ Prodapt.CSharp.Day5/RLE.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Prodapt.CSharp.Day5
+{
+    internal class RLE
+    {
+        public string Encode(string message)
+        {
+            string encodedMessage="";
+            int count = 1;
+
+            for (int i = 1; i < message.Length; i++)
+            {
+                if (message[i] == message[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    encodedMessage += message[i - 1].ToString();
+                    encodedMessage += count.ToString();
+                    count = 1;
+                }
+            }
+
+            encodedMessage += message[message.Length - 1].ToString();
+            encodedMessage += count.ToString();
+
+            return encodedMessage;
+        }
+
+        static void Main(string[] args)
+        {
+            RLE obj = new();
+            var result = obj.Encode("AABAB");
+            Console.WriteLine(result);
+        }
+    }
+}
