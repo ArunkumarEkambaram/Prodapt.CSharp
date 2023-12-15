e83e59f10b6d00c685ce0668813585c18e02d780
ArunKumar <arunkumar.e@outlook.com>
2023-12-15 09:40:47 +0530

    updated on 14-12-2023

---
+++ Prodapt.CSharp.Day5/ParticipantsApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Prodapt.ExerciseLibrary;
+
+namespace Prodapt.CSharp.Day5
+{
+    internal class ParticipantsApp
+    {
+        static void Main(string[] args)
+        {
+            Participants obj = new Participants(1001, "Kavin", 99.5, 98.6, 97.6);
+            obj.CalculateTotalObtainedMarks();
+            obj.CalculatePercentage();
+            Console.WriteLine($"----------- {Participants.companyName}------------");
+            var result = obj.DisplayParticipants();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Prodapt.ExerciseLibrary/Participants.cs
@@ -0,0 +1,46 @@
+﻿namespace Prodapt.ExerciseLibrary
+{
+    public class Participants
+    {
+        public static string companyName;
+        int empId;
+        string name;
+        double foundationMarks, webBasicMarks, dotnetMarks, totalMarks, obtainedMark, percentage;
+
+        //Static Constructor
+        static Participants()
+        {
+            companyName = "Corporate Unniversity";
+        }
+
+        public Participants() { }
+
+        //Initialize all the parameters
+        public Participants(int empId, string name, double foundationMarks, double webBasicMarks, double dotnetMarks)
+        {
+            this.empId = empId;
+            this.name = name;
+            this.foundationMarks = foundationMarks;
+            this.webBasicMarks = webBasicMarks;
+            this.dotnetMarks = dotnetMarks;
+            this.totalMarks = 300;
+        }
+
+        //Calculate Obtained marks
+        public void CalculateTotalObtainedMarks()
+        {
+            obtainedMark = foundationMarks + webBasicMarks + dotnetMarks;
+        }
+
+        public void CalculatePercentage()
+        {
+            percentage = obtainedMark / totalMarks * 100;
+        }
+
+        public string DisplayParticipants()
+        {
+            return $"Emp Id :{empId}\nName :{name}\nFoundation Mark :{foundationMarks}\nWeb Basic Mark :{webBasicMarks}\nDotnet mark:{dotnetMarks}" +
+                $"\nObtained Mark :{obtainedMark}\nPercentage :{percentage}";
+        }
+    }
+}
