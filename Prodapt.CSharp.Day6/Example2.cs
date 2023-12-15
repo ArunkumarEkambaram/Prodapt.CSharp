9bb90c5ce7ad6315da82662dd226ea5d5a84b9c3
ArunKumar <arunkumar.e@outlook.com>
2023-12-15 12:46:17 +0530

    Created Day6 Project

---
+++ Prodapt.CSharp.Day6/Example2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prodapt.CSharp.Day6
+{
+    public class Example2
+    {
+        //array in parameter
+        public void GetElements(int[] elements, string[] names)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.WriteLine(elements[i]);
+            }
+            Console.WriteLine(names[2]);
+        }
+        public int[] GetValues(int[] values)
+        {
+            return new int[] { 2, 3, 4, 5 };
+        }
+        static void Main(string[] args)
+        {
+            Example2 obj = new Example2();
+            int[] t1 = { 1, 3, 5, 6, 7 };
+            string[] names = { "a", "b", "c" };
+            obj.GetElements(t1, names);
+            var output = obj.GetValues(t1);           
+            Console.WriteLine(string.Join("-", output));
+            // obj.GetElements(new int[] { 1, 2, 3, 4, 5 });
+            //obj.GetElements(new[] { 3, 33, 333, 3333 }, new[] { "Sanjai", "Karthick", "Lokesh" });
+        }
+    }
+}
---
+++ Prodapt.CSharp.Day6/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Prodapt.CSharp.Day6
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            int[] arr = new int[10];
+            arr[0] = 10;
+            arr[2] = 34;
+            arr[1] = 23;
+            arr[3] = 6;
+            arr[5] = 55;
+            arr[6] = -8;
+
+            string[] cities = new string[3] { "Chennai", "Bengaluru", "Madurai" };
+
+            string[] names = new string[] { "Prabhu", "Kumar" };
+
+            //Accept value from User using For Loop
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter a number :");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Using For Loop");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine($"Index :{i} is {arr[i]}");
+            //}
+
+            //Sorting
+            // Array.Sort(arr);
+
+            //Reverse
+            //Array.Reverse(arr);
+
+            //Copy array from another
+            // int[] arr2 = arr;
+            int[] arr2 = new int[4];
+            arr2 = arr;
+
+            //Array.Copy(arr, arr2, 3);
+            //Array.Copy(arr, 2, arr2, 3, 1);
+
+            //Resize
+            //Array.Resize(ref arr, 20);
+
+            Console.WriteLine("Using Foreach Loop");
+            foreach (int x in arr2)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
